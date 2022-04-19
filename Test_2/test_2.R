c31e04de8572f87ec71efb21bc97a9141f8cc331
adarshgowdaa <adarshgowda463@gmail.com>
2022-04-19 18:11:53 +0530

    Updated Test-2

--- Test_2/test_2.R
+++ Test_2/test_2.R
@@ -1,26 +1,30 @@
 convert <- function(input_file, title, bib_file = NULL){
   
   #Converting from LaTeX to Markdown
-  file_1 = paste(toString(tools::file_path_sans_ext(input_file)), ".md", sep = "", collapse = NULL)
+  path = toString(tools::file_path_sans_ext(input_file))
+  file_1 = paste(path, ".md", sep = "", collapse = NULL)
   rmarkdown::pandoc_convert(input_file, to = "markdown", output = file_1)
   
+  #Creating New outputs directory
+  dir.create("outputs")
+  output_file = paste("outputs/", path,".Rmd",sep="")
+  
   #Edit YAML Header
+  data = paste("---","\n","title:",title,"\n","bibliography:",bib_file,"\n","output:","rjtools::rjournal_web_article","\n", sep = " ")
+  sink(output_file, append = TRUE)
+  cat(data)
+  cat("---")
+  cat("\n")
+  sink()
+  
+  #Copying Other Contents
   file_2 = file(file_1, open = "rt")
   input_lines <- readLines(file_1)
-  header = "---"
-  title_tag = "title:"
-  bib = "bibliography:"
-  output = "output:"
-  set_to = "rjtools::rjournal_web_article"
-  
-  #Creating New outputs directory
-  dir.create("outputs", showWarnings = FALSE)
-  output_file = file(paste("outputs/",toString(tools::file_path_sans_ext(input_file)),".Rmd",sep=""), open="wt")
-  writeLines(paste(paste(header,"\n",title_tag,title,"\n",bib,bib_file,"\n",output,set_to,"\n", sep = " "),""),con = output_file, useBytes = FALSE)
-  writeLines(paste(header,""),con=output_file, useBytes = FALSE)
-  writeLines(paste(input_lines,""),con=output_file, useBytes = FALSE)
-  close.connection(file_2)
-  close.connection(output_file)
+  data = paste(input_lines, sep = "")
+  sink(output_file, append = TRUE)
+  cat(data)
+  sink()
+  close(file_2)
   
   #Copying Required Files
   
